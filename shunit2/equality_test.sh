51a4b20ef3b4111e54b5ce47ed3abab3f6ebae90
assout <porinsan@gmail.com>
2015-07-26 18:10:40 +0900

    change travis setting

--- shunit2/equality_test.sh
+++ shunit2/equality_test.sh
@@ -9,7 +9,7 @@ testEqualityFail() {
 # load shunit2 TODO workaround for travisci
 shunit2Paths=("shunit2-2.1.6/src/shunit2", "/usr/share/shunit2/shunit2")
 for shunit2Path in ${shunit2Paths[@]} ; do
-	if [ -r ${shunit2Path} ] ; then
+	if [ -e ${shunit2Path} ] ; then
 		. ${shunit2Path}
 		exit 0
 	fi
